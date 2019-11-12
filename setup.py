0d7fa483101ac31b67f4a3b4f728a3b570afe880
jshleap <jshleap@squalus.org>
2019-11-12 17:51:30 -0500

    modifying requirements in setup

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8'
+          ) as r:
+    requirements = r.read().strip().split()
 setuptools.setup(
     name='justblast',
     version=version,
@@ -17,10 +21,7 @@ setuptools.setup(
                 'systems, as well as rough taxonomomic annoation using BASTA '
                 'LCA',
     python_requires='>=3.6',
-    install_requires=['cycler==0.10.0', 'joblib==0.14.0', 'kiwisolver==1.1.0',
-                      'matplotlib==3.1.1', 'numpy==1.17.4', 'pandas==0.25.3',
-                      'plyvel==1.1.0', 'pyparsing==2.4.5', 'pytz==2019.3',
-                      'python-dateutil==2.8.1', 'six==1.13.0', 'tqdm==4.38.0'],
+    install_requires=[requirements],
     dependency_links=['https://github.com/timkahlke/BASTA#BASTA'],
     long_description=long_description,
     long_description_content_type='text/markdown'
