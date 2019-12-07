3a44f33c831f0e361a65a97a90891f29185b5482
jshleap <jshleap@squalus.org>
2019-12-06 20:09:29 -0500

    Adding uniques

--- justblast/__init__.py
+++ justblast/__init__.py
@@ -4,4 +4,5 @@ import os
 
 thisdir = os.path.split(os.path.realpath(__file__))[0]
 itlist = os.listdir(thisdir)
-__all__ = [os.path.split(x)[-1].strip('.py') for x in itlist if x.endswith('.py') and not x.endswith('__init__.py')]
+__all__ = [os.path.split(x)[-1].strip('.py') for x in itlist if x.endswith(
+    '.py') and not x.endswith('__init__.py')]
