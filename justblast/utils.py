107a8a66446b1342e56f7f7971b09b72cc4ff513
jshleap <jshleap@squalus.org>
2020-01-08 09:15:02 -0500

    Including main in git

--- justblast/utils.py
+++ justblast/utils.py
@@ -23,7 +23,7 @@ import shelve
 from itertools import zip_longest
 from subprocess import run, PIPE, CalledProcessError
 from typing import Optional, Iterator, Tuple, List
-
+from textwrap import wrap
 import dill
 import matplotlib.pyplot as plt
 import numpy as np
@@ -62,6 +62,8 @@ class FastX(object):
     bigfile: bool = False
     uniques: List[str] = []
     n_duplicates: int = 0
+    handle = None
+    store = None
 
     def __init__(self, filename: str, trimm: Optional[int] = None,
                  outprefix: str = None, unique: bool = False,
@@ -73,15 +75,19 @@ class FastX(object):
         :param outprefix: Prefix or output
         """
         self.filename = filename
+        self.outprefix = outprefix
+        self.shelve_name = '%s.shelf' % self.outprefix
         self.unique = unique
         self.trimm = trimm
         self.tipo = filename
-        self.outprefix = outprefix
         self.cpus = cpus
         self.populate()
 
     def __str__(self):
         return '\n'.join(self.yield_seq(as_fasta=self.tipo == 'a'))
+
+    def __len__(self):
+        return len(self.ids)
 
     @property
     def tipo(self):
@@ -103,6 +109,15 @@ class FastX(object):
             self._tipo = 'q'
         else:
             raise Exception("Not an acceptable file format")
+        conversion =(1024 * 1024 * 1024)
+        self.handle = self.open(filename)
+        file_size = os.fstat(self.handle.fileno()).st_size / conversion
+        avail_mem = virtual_memory().available / conversion
+        if file_size <= avail_mem:
+            self.store = {}
+        else:
+            self.store = shelve.open(self.shelve_name)
+            self.bigfile = True
 
     @property
     def outprefix(self):
@@ -121,57 +136,45 @@ class FastX(object):
             self.parse_fastq(self.filename)
 
     def yield_seq(self, as_fasta: bool = True) -> Iterator[str]:
-        if self.bigfile:
-            f = shelve.open(self.shelve_name)
-        else:
-            f = self.shelve_name
         for name in self.ids:
-            seq = f[name][0]
-            if self.unique and seq in self.uniques:
+            seq = self.store[name][0]
+            if self.unique and (seq in self.uniques):
                 self.n_duplicates += 1
                 continue
             if as_fasta:
-                seq = '>%s\n%s' % (name, seq)
+                name = name.replace('@', '')
+                seq = '>%s\n%s' % (name, '\n'.join(wrap(seq, 80)))
             else:
                 seq = '@%s\n%s\n+\n%s' % (name, seq, seq)
             self.uniques.append(seq)
             yield seq
         if self.bigfile:
-            f.close()
+            self.store.close()
+        if self.unique:
+            print(self.n_duplicates, 'duplicates found and removed')
 
     def parse_fastq(self, file_name: str) -> None:
-        self.shelve_name = '%s.shelf' % self.outprefix
-        with self.open(file_name) as handle:
-            file_size = os.fstat(handle.fileno()).st_size / (
-                    1024 * 1024 * 1024)
-            avail_mem = virtual_memory().available / (1024 * 1024 * 1024)
-            if file_size <= avail_mem:
-                shelf = {}
-            else:
-                shelf = shelve.open(self.shelve_name)
-                self.bigfile = True
-            args = [iter(handle)] * 4
-            if not os.path.isfile('%s.shelve' % self.outprefix):
-                aln = Parallel(n_jobs=self.cpus)(  # , prefer="threads")(
-                    delayed(self.run_parser_fastq)(
-                        chunk, self.trimm, shelf, self.bigfile)
-                    for chunk in tqdm(zip_longest(*args, fillvalue=None),
-                                      desc="Parsing %s" % file_name))
-                if self.bigfile:
-                    self.ids, self.lengths = zip(*aln)
-                else:
-                    self.ids, self.lengths, tuples = zip(*aln)
-                    shelf = dict(tuples)
-            elif not self.bigfile:
-                with open(self.shelve_name, 'rb') as d:
-                    shelf = dill.load(d)
-
+        args = [iter(self.handle)] * 4
+        if not os.path.isfile('%s.shelve' % self.outprefix):
+            aln = Parallel(n_jobs=self.cpus)(  # , prefer="threads")(
+                delayed(self.run_parser_fastq)(
+                    chunk, self.trimm, self.store, self.bigfile)
+                for chunk in tqdm(zip_longest(*args, fillvalue=None),
+                                  desc="Parsing %s" % file_name))
             if self.bigfile:
-                shelf.close()
+                self.ids, self.lengths = zip(*aln)
             else:
-                with open(self.shelve_name, 'wb') as d:
-                    dill.dump(shelf, d)
-                self.shelve_name = shelf
+                self.ids, self.lengths, tuples = zip(*aln)
+                self.store = dict(tuples)
+        elif not self.bigfile:
+            with open(self.shelve_name, 'rb') as d:
+                self.store = dill.load(d)
+
+        if self.bigfile:
+            self.store.close()
+        else:
+            with open(self.shelve_name, 'wb') as d:
+                dill.dump(self.store, d)
 
     @staticmethod
     def run_parser_fastq(chunk, trimm, shelf, big):
@@ -190,26 +193,22 @@ class FastX(object):
 
     def parse_fasta(self, file_name: str) -> None:
         name, seq = None, ''
-        fn = file_name[: file_name.rfind('.')]
-        self.shelve_name = '%s.shelf' % fn
-        with shelve.open(self.shelve_name) as shelf, self.open(
-                file_name) as handle:
-            for line in tqdm(handle, desc="Parsing %s" % file_name):
-                line = line.decode('utf-8').strip() if isinstance(
-                    line, bytes) else line.strip()
-                if line.startswith(">"):
-                    if name:
-                        seq = seq if self.trimm is None else seq[:self.trimm]
-                        shelf[name] = (seq, None)
-                        self.ids = np.append(self.ids, name)
-                    name = line[1:]
-                    seq = ''
-                else:
-                    seq += line
-            if name:
-                seq = seq if self.trimm is None else seq[:self.trimm]
-                shelf[name] = (seq, '')
-                self.ids = np.append(self.ids, name)
+        for line in tqdm(self.handle, desc="Parsing %s" % file_name):
+            line = line.decode('utf-8').strip() if isinstance(
+                line, bytes) else line.strip()
+            if line.startswith(">"):
+                if name:
+                    seq = seq if self.trimm is None else seq[:self.trimm]
+                    self.store[name] = (seq, None)
+                    self.ids = np.append(self.ids, name)
+                name = line[1:]
+                seq = ''
+            else:
+                seq += line
+        if name:
+            seq = seq if self.trimm is None else seq[:self.trimm]
+            self.store[name] = (seq, '')
+            self.ids = np.append(self.ids, name)
 
     def write(self, fasta: bool = True) -> None:
         if fasta:
